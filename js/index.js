667363f7d6558bfdf516350d6d9f866b132546df
Katarzyna Mazur <mazur66@hotmail.com>
2020-10-16 23:12:19 -0400

    Added js folder and file and started adding js for the hamburger functionality

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const navToggle = document.querySelector('.nav-toggle');
+//const navLinks = document.querySelectorAll('.nav__list');
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+
+
+
