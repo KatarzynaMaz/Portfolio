c2ca193fb9180124290f2fbaede234afeeac7f6b
Katarzyna Mazur <mazur66@hotmail.com>
2020-10-18 14:15:03 -0400

    Closing navigation when going to the link

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
 const navToggle = document.querySelector('.nav-toggle');
-//const navLinks = document.querySelectorAll('.nav__list');
+const navLinks = document.querySelectorAll('.nav__link');
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 });
 
-
-
-
+//when we have the navigation open and we click on a link, it will take us to the link
+//and will close the navigation
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+      document.body.classList.remove('nav-open');
+})
+})
